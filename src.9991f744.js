81cb1428fd71c84be771b1418a6b756cf4d18667
shmetanelena <92665632+shmetanelena@users.noreply.github.com>
2022-01-14 07:31:55 +0000

    Deploying to gh-pages from @ shmetanelena/go-sudoy-team-project@a2d8713a734aa66caa512231d4bfda8abe783891 🚀

--- src.9991f744.js
+++ src.9991f744.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/go-sudoy-team-project/src.02ca8848.js.map
+//# sourceMappingURL=/go-sudoy-team-project/src.9991f744.js.map
